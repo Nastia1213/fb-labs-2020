eaaf7e6e663bb6e30d05fab1b25ffd8b676d5878
Nastia1213 <nastia.danevych.fb84.com>
2020-11-11 00:38:08 +0200

    [CP_2] Danevuch (FB-84)

--- cp_2/grigoryan_fb84_biletskiy_fb84_cp2/filter.py
+++
@@ -1,5 +0,0 @@
-with open("text.txt",'r',encoding='utf-8') as rf:
-    lines = rf.readlines()
-    lines = [line.replace('\n', '') for line in lines]
-with open("text.txt",'w',encoding='utf-8') as rf:
-    rf.writelines(lines)
